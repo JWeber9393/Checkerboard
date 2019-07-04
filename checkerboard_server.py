2074fd3f25b3013797c74cc64087045412975bcf
Joey Weber <weberjoseph9393@yahoo.com>
2019-07-04 14:46:52 -0700

    html redone, table added

--- checkerboard_server.py
+++ checkerboard_server.py
@@ -7,11 +7,17 @@ def checkerboard():
     print("root route good!")
     return render_template("checkerboard.html")
 
-@app.route("/<num>")
-def checkerboard2(num):
+# @app.route("/4")
+# def checkerboard2():
+#     print('*'*100)
+#     print("num route good!")
+#     return render_template("checkerboard.html")
+
+@app.route("/<x>/<y>")
+def checkerboard3(x, y):
     print('*'*100)
     print("num route good!")
-    return render_template("checkerboard.html", num_times = int(num))
+    return render_template("checkerboard.html", x = int(x), y = int(y))
 
 if __name__=="__main__":
     app.run(debug = True)
